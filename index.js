428326f059198a958e2a42b6c37d2828dcb2eb18
maximosigniorini <maximosigniorini97@gmail.com>
2020-04-24 23:11:40 -0300

    Se termina de implementar la reproduccion dinamica de audios

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const {prefix,token} = require('./config.json');
 const client = new Discord.Client();
 let isReady = true;
+let sonidos = ["Chano","aparezco","buenisimo","fino","niki","a4","aplaudo","cortito","hola","privado","acho","basta","cpiko","jaram","skate","achotapita","boa","dios","love","traicionera","ahre","bob","filisteo","marina"]
 
 client.once('ready', () => {
   console.log("Ready!");
@@ -11,48 +12,52 @@ client.once('ready', () => {
 //Funcionamiento central del bot
 client.on('message', async message => {
 const voiceChannel = await message.member.voice.channel;
+let miMensaje = message.content
+miMensaje = miMensaje.slice(2)
 
-  switch(message.content){
+for(let i = 0;i < sonidos.length;i++){
 
-    case '! traicionera':
+  if(miMensaje == sonidos[i]){
+    isReady = false;
+       voiceChannel.join().then(connection => {
+         const dispatcher = connection.play('./Audio/' + sonidos[i] + '.ogg');
+         dispatcher.on("end", end => {
+           voiceChannel.leave();
+         })
+         }).catch(err => console.log(err));
 
-      isReady = false;
-      voiceChannel.join().then(connection => {
-        const dispatcher = connection.play('./Audio/traicionera.ogg');
-        dispatcher.on("end", end => {
-          voiceChannel.leave();
-        })
-        }).catch(err => console.log(err));
+         isReady = true;
+   }
+}
 
-        isReady = true;
-
-      break;
-
-    case '! acho':
-
-      isReady = false;
-      voiceChannel.join().then(connection => {
-        const dispatcher = connection.play('./Audio/acho.ogg');
-        dispatcher.on("end", end => {
-        voiceChannel.leave();
-      })
-        }).catch(err => console.log(err));
-        isReady = true;
-
-      break;
-  }
-
-  // if (isReady && message.content.startsWith(`${prefix} traicionera`)){
-  //   isReady = false;
+  // switch(message.content){
   //
-  //   const voiceChannel = await message.member.voice.channel;
-  //   voiceChannel.join().then(connection => {
-  //     const dispatcher = connection.play('./Audio/traicionera.ogg');
-  //     dispatcher.on("end", end => {
-  //     voiceChannel.leave();
-  //   })
-  //     }).catch(err => console.log(err));
-  //     isReady = true;
+  //   case '! traicionera':
+  //
+  //     isReady = false;
+  //     voiceChannel.join().then(connection => {
+  //       const dispatcher = connection.play('./Audio/traicionera.ogg');
+  //       dispatcher.on("end", end => {
+  //         voiceChannel.leave();
+  //       })
+  //       }).catch(err => console.log(err));
+  //
+  //       isReady = true;
+  //
+  //     break;
+  //
+  //   case '! acho':
+  //
+  //     isReady = false;
+  //     voiceChannel.join().then(connection => {
+  //       const dispatcher = connection.play('./Audio/acho.ogg');
+  //       dispatcher.on("end", end => {
+  //       voiceChannel.leave();
+  //     })
+  //       }).catch(err => console.log(err));
+  //       isReady = true;
+  //
+  //     break;
   // }
 })
 
