7abc751b612d0fcaa8e3a9855af2abc06e100fa3
maximosigniorini <maximosigniorini97@gmail.com>
2020-04-24 21:42:01 -0300

    Se comienza a hacer mas dinamica la reproduccion de audios

--- index.js
+++ index.js
@@ -7,21 +7,53 @@ client.once('ready', () => {
   console.log("Ready!");
 })
 
+
 //Funcionamiento central del bot
 client.on('message', async message => {
+const voiceChannel = await message.member.voice.channel;
 
-  if (isReady && message.member.voice.channel){
-    isReady = false;
+  switch(message.content){
 
-    const voiceChannel = await message.member.voice.channel;
-    voiceChannel.join().then(connection => {
-      const dispatcher = connection.play('./Audio/test.wav');
-      dispatcher.on("end", end => {
-      voiceChannel.leave();
-    })
-      }).catch(err => console.log(err));
-      isReady = true;
+    case '! traicionera':
+
+      isReady = false;
+      voiceChannel.join().then(connection => {
+        const dispatcher = connection.play('./Audio/traicionera.ogg');
+        dispatcher.on("end", end => {
+          voiceChannel.leave();
+        })
+        }).catch(err => console.log(err));
+
+        isReady = true;
+
+      break;
+
+    case '! acho':
+
+      isReady = false;
+      voiceChannel.join().then(connection => {
+        const dispatcher = connection.play('./Audio/acho.ogg');
+        dispatcher.on("end", end => {
+        voiceChannel.leave();
+      })
+        }).catch(err => console.log(err));
+        isReady = true;
+
+      break;
   }
+
+  // if (isReady && message.content.startsWith(`${prefix} traicionera`)){
+  //   isReady = false;
+  //
+  //   const voiceChannel = await message.member.voice.channel;
+  //   voiceChannel.join().then(connection => {
+  //     const dispatcher = connection.play('./Audio/traicionera.ogg');
+  //     dispatcher.on("end", end => {
+  //     voiceChannel.leave();
+  //   })
+  //     }).catch(err => console.log(err));
+  //     isReady = true;
+  // }
 })
 
 client.login(token);
